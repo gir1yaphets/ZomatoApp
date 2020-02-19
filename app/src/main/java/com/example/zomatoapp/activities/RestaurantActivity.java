4b98d8056dbb28393cb5f76c79729bddf02b2f6b
gir1yaphets <763791240@qq.com>
2020-02-18 17:19:05 -0800

    Update detail features in the restaurant activity

--- app/src/main/java/com/example/zomatoapp/activities/RestaurantActivity.java
+++ app/src/main/java/com/example/zomatoapp/activities/RestaurantActivity.java
@@ -6,8 +6,11 @@ import com.example.zomatoapp.R;
 import com.example.zomatoapp.dataModel.RestaurantModel;
 import com.example.zomatoapp.databinding.ActivityRestaurantBinding;
 import com.example.zomatoapp.eventbus.OnRestaurantsSuccessEvent;
+import com.example.zomatoapp.ui.HighlightListAdapter;
 import com.example.zomatoapp.utils.StaticValues;
+import com.example.zomatoapp.viewModel.HighlightItemViewModel;
 import com.example.zomatoapp.viewModel.RestActivityViewModel;
+import com.example.zomatoapp.viewModel.RestDetailViewModel;
 import com.example.zomatoapp.viewModel.RestMenuViewModel;
 import com.example.zomatoapp.viewModel.RestTitleViewModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,16 +24,24 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class RestaurantActivity extends AppCompatActivity implements OnMapReadyCallback {
     private ActivityRestaurantBinding mBinding;
     private RestActivityViewModel mViewModel;
 
     private SupportMapFragment mapFragment;
+
+    private RecyclerView highlightRecyclerView;
+    private HighlightListAdapter highLightAdapter;
 
     private static final LatLng SFO = new LatLng(37.614631, -122.385153);
     private static final LatLng SOH = new LatLng(-33.85704, 151.21522);
@@ -40,6 +51,7 @@ public class RestaurantActivity extends AppCompatActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_restaurant);
+        initView();
         initViewModel();
 
         mBinding.setViewModel(mViewModel);
@@ -52,10 +64,19 @@ public class RestaurantActivity extends AppCompatActivity implements OnMapReadyC
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     }
 
+    private void initView() {
+        highlightRecyclerView = mBinding.llDetailLayout.rvHighlight;
+        highlightRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        highLightAdapter = new HighlightListAdapter(new ArrayList<>());
+        highLightAdapter.setData(new ArrayList<>());
+        highlightRecyclerView.setAdapter(highLightAdapter);
+    }
+
     private void initViewModel() {
         mViewModel = new ViewModelProvider(this).get(RestActivityViewModel.class);
         mViewModel.titleViewModel = new ViewModelProvider(this).get(RestTitleViewModel.class);
         mViewModel.menuViewModel = new ViewModelProvider(this).get(RestMenuViewModel.class);
+        mViewModel.detailViewModel = new ViewModelProvider(this).get(RestDetailViewModel.class);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -66,6 +87,9 @@ public class RestaurantActivity extends AppCompatActivity implements OnMapReadyC
 
         //Update restaurant title
         updateRestTitle(restaurantModel);
+
+        //Update restaurant detail
+        updateRestDetail(restaurantModel);
 
         mapFragment.getMapAsync(this);
     }
@@ -78,6 +102,19 @@ public class RestaurantActivity extends AppCompatActivity implements OnMapReadyC
         mViewModel.titleViewModel.restReviewCount.setValue(restaurantModel.getAllReviewsCount() + " reviews");
     }
 
+    private void updateRestDetail(RestaurantModel restaurantModel) {
+        mViewModel.detailViewModel.cuisines.setValue(restaurantModel.getCuisines());
+        mViewModel.detailViewModel.averageCost.setValue(restaurantModel.getAverageCostForTwo() + "$ for two people");
+
+        for (String highlightInfo : restaurantModel.getHighlights()) {
+            HighlightItemViewModel itemViewModel = new HighlightItemViewModel();
+            itemViewModel.highlighName.setValue(highlightInfo);
+            mViewModel.detailViewModel.addHighlightItem(itemViewModel);
+        }
+
+        List<HighlightItemViewModel> list =  mViewModel.detailViewModel.getHighlightItemViewModels();
+        highLightAdapter.setData(list);
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
--- app/src/main/java/com/example/zomatoapp/viewModel/RestActivityViewModel.java
+++ app/src/main/java/com/example/zomatoapp/viewModel/RestActivityViewModel.java
@@ -19,6 +19,7 @@ public class RestActivityViewModel extends AndroidViewModel {
     public MutableLiveData<LocationModel> location = new MutableLiveData<>();
     public RestTitleViewModel titleViewModel;
     public RestMenuViewModel menuViewModel;
+    public RestDetailViewModel detailViewModel;
 
     public RestActivityViewModel(@NonNull Application application) {
         super(application);
