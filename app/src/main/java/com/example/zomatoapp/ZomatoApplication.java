641772db446c9d33d031f7d1ab7fd4b77dc53b60
gir1yaphets <763791240@qq.com>
2020-03-07 15:45:38 -0800

    Add firebase simple background message

--- app/src/main/java/com/example/zomatoapp/ZomatoApplication.java
+++ app/src/main/java/com/example/zomatoapp/ZomatoApplication.java
@@ -2,14 +2,44 @@ package com.example.zomatoapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import androidx.annotation.NonNull;
 
 public class ZomatoApplication extends Application {
+    private static final String TAG = ZomatoApplication.class.getName();
     private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+
+        initializeCloudMessaging();
+    }
+
+    private void initializeCloudMessaging() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        Log.d(TAG, "token = " + token);
+                    }
+                });
     }
 
     public static Context getContext() {
