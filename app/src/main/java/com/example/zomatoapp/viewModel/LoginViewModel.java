cb8046d3ee200d8b6efd90f1144ece84bfca4511
gir1yaphets <763791240@qq.com>
2020-03-10 15:15:39 -0700

    add sign up successful toast

--- app/src/main/java/com/example/zomatoapp/viewModel/LoginViewModel.java
+++ app/src/main/java/com/example/zomatoapp/viewModel/LoginViewModel.java
@@ -71,6 +71,9 @@ public class LoginViewModel extends AndroidViewModel {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
                                 FirebaseUser user = auth.getCurrentUser();
+
+                                Toast.makeText(getApplication(), "Authentication Successful!.",
+                                        Toast.LENGTH_SHORT).show();
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
